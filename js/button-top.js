86639d69bfed39e5cb13b181056fa5987a8f5967
Papp <papp.oliver188@gmail.com>
2020-03-11 11:51:59 +0100

    update

--- js/button-top.js
+++ js/button-top.js
@@ -1,21 +1,19 @@
-//Get the button:
 var buttonTop = document.getElementById('buttonTop');
-//Get the height of header (min. 100vh):
-var headerHeight = document.getElementById('header');
+var header = document.getElementById('header');
 
-// When the user left behind the header from the top of the document, show the button
+// When the user left behind the #header, show the button.
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > headerHeight.scrollHeight || document.documentElement.scrollTop > headerHeight.scrollHeight) {
+  if (document.body.scrollTop >= header.scrollHeight || document.documentElement.scrollTop >= header.scrollHeight) {
     buttonTop.style.display = 'block';
   } else {
     buttonTop.style.display = 'none';
   }
 }
 
-// When the user clicks on the div, scroll to the top of the document
+// When the user clicks on the #buttonTop, scroll to the top of the document.
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // Safari
+  document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
 }
